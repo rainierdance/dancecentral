96067c2a738171f3e84242c84bc818c9995494ab
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-01-25 23:15:12 +0000

    Initial Cha Cha and Samba figure names (no follows yet)

--- trunk/chacha.js
+++ trunk/chacha.js
@@ -1,35 +1,254 @@
     //-------------------------------------------
     // CHACHA FIGURES
-    var chachaFigures = {
-        'Cucarachas' : 
+/*
+        'foo' : 
           {
-            'name' : 'Cucarachas',
-            'urlpath' : '/cucarachas',
-            'level' : 'Newcomer',
+            'name' : 'foo',
+            'urlpath' : '/foo',
+            'level' : 'Gold',
             'follow' : [
-                {
-                   'id' : 'SideSteps'
-                },
-                {
-                   'id' : 'Fan',
-                   'comment' : 'for LF Cucaracha, go directly to 4-6 of Fan, lady in line on 4'
-                }
               ]
           },
-        'BasicMovementsOpen' : 
-         {
-            'name' : 'Basic Movements: Open',
-            'urlpath' : '/basic-movements',
+*/
+    var chachaFigures = {
+        'FootChanges' : 
+          {
+            'name' : 'Foot Changes',
+            'level' : 'Gold',
+            'follow' : [
+              ]
+          },
+        'FollowMyLeader' : 
+          {
+            'name' : 'Follow My Leader',
+            'level' : 'Gold',
+            'follow' : [
+              ]
+          },
+        'Sweetheart' : 
+          {
+            'name' : 'Sweetheart',
+            'level' : 'Gold',
+            'follow' : [
+              ]
+          },
+        'TurkishTowel' : 
+          {
+            'name' : 'Turkish Towel',
+            'level' : 'Gold',
+            'follow' : [
+              ]
+          },
+        'HipTwistSpiral' : 
+          {
+            'name' : 'Hip Twist Spiral',
+            'level' : 'Gold',
+            'follow' : [
+              ]
+          },
+        'AdvancedHipTwist' : 
+          {
+            'name' : 'Advanced Hip Twist',
+            'level' : 'Gold',
+            'follow' : [
+              ]
+          },
+        'Chase' : 
+          {
+            'name' : 'Chase',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'Cuban Breaks' : 
+          {
+            'name' : 'Cuban Breaks',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'CrossBasic' : 
+          {
+            'name' : 'Cross Basic',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'RopeSpinning' : 
+          {
+            'name' : 'Rope Spinning',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'Curl' : 
+          {
+            'name' : 'Curl',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'Spiral' : 
+          {
+            'name' : 'Spiral',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'Aida' : 
+          {
+            'name' : 'Aida',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'OpeningOutFromReverseTop' : 
+          {
+            'name' : 'Opening Out From Reverse Top',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'ReverseTop' : 
+          {
+            'name' : 'Reverse Top',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'OpenHipTwist' : 
+          {
+            'name' : 'Open Hip Twist',
+            'level' : 'Silver',
+            'follow' : [
+              ]
+          },
+        'ClosedHipTwist' : 
+          {
+            'name' : 'Closed Hip Twist',
+            'urlpath' : '/closed-hip-twist',
+            'level' : 'Bronze',
+            'follow' : [
+              ]
+          },
+        'NaturalOpeningOutMovement' : 
+          {
+            'name' : 'Natural Opening Out Movement',
+            'urlpath' : '/natural-opening-out-movement',
+            'level' : 'Bronze',
+            'follow' : [
+              ]
+          },
+        'NaturalTop' : 
+          {
+            'name' : 'Natural Top',
+            'urlpath' : '/natural-top',
+            'level' : 'Bronze',
+            'follow' : [
+              ]
+          },
+        'HockeyStick' : 
+          {
+            'name' : 'Hockey Stick',
+            'urlpath' : '/hockey-stick',
+            'level' : 'Bronze',
+            'follow' : [
+              ]
+          },
+        'Alemana' : 
+          {
+            'name' : 'Alemana',
+            'urlpath' : '/alemana',
+            'level' : 'Bronze',
+            'follow' : [
+              ]
+          },
+        'Fan' : 
+          {
+            'name' : 'Fan',
+            'urlpath' : '/fan',
+            'level' : 'Bronze',
+            'follow' : [
+              ]
+          },
+        'TimeSteps' : 
+          {
+            'name' : 'Time Steps',
+            'urlpath' : '/time-step',
             'level' : 'Newcomer',
             'follow' : [
-                {
-                   'id' : 'ProgressiveWalks',
-                   'comment' : 'back, after 1-3, when danced in Open or Closed Position'
-                },
-                {
-                   'id' : 'NaturalTop',
-                   'comment' : 'after 1-3 Open Basic, Man step 3 LF side turning slightly R, Lady RF fwd, danced in Open Position'
-                }
+              ]
+          },
+        'ThereAndBack' : 
+          {
+            'name' : 'There And Back',
+            'urlpath' : '/there-and-back',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'SideSteps' : 
+          {
+            'name' : 'Side Steps',
+            'urlpath' : '/side-step',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'ThreeChaChaChas' : 
+          {
+            'name' : 'Three Cha Cha Chas',
+            'urlpath' : '/three-cha-cha-chas',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'HandToHand' : 
+          {
+            'name' : 'Hand To Hand',
+            'urlpath' : '/hand-to-hand',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'ShoulderToShoulder' : 
+          {
+            'name' : 'Shoulder To Shoulder',
+            'urlpath' : '/shoulder-to-shoulder',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'UnderarmTurn' : 
+          {
+            'name' : 'Underarm Turn',
+            'urlpath' : '/spot-turn',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'SpotTurn' : 
+          {
+            'name' : 'Spot or Switch Turn',
+            'urlpath' : '/spot-turn',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'NewYork' : 
+          {
+            'name' : 'New York',
+            'urlpath' : '/new-york',
+            'level' : 'Newcomer',
+            'follow' : [
+              ]
+          },
+        'BasicMovements' : 
+         {
+            'name' : 'Basic Movements',
+            'urlpath' : 'cha-cha-basic',
+            'level' : 'Newcomer',
+            'follow' : [
              ]
         }
     };
--- trunk/samba.js
+++ trunk/samba.js
@@ -1,25 +1,238 @@
     //-------------------------------------------
     // SAMBA FIGURES
-    var sambaFigures = {
-        'HipTwists' : 
-          {
-            'name' : 'Hip Twists (Advanced, Continuous and Circular)',
-            'level' : 'Gold'
-          },
-        'BasicMovementsOpen' : 
+/*
+        'foo' : 
          {
-            'name' : 'Basic Movements: Open',
+            'name' : 'foo',
+            'urlpath' : '/foo',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+*/
+    var sambaFigures = {
+        'CruzadosWalksAndLocks' : 
+         {
+            'name' : 'Cruzados Walks and Locks',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'SambaLocks' : 
+         {
+            'name' : 'Samba Locks',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ThreeStepTurn' : 
+         {
+            'name' : 'Three Step Turn',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'PromenadeAndCPPRuns' : 
+         {
+            'name' : 'Promenade And Counter Promenade Runs',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ReverseRoll' : 
+         {
+            'name' : 'Reverse Roll',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'NaturalRoll' : 
+         {
+            'name' : 'Natural Roll',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'Roundabout' : 
+         {
+            'name' : 'Round About',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ContraBotaFogos' : 
+         {
+            'name' : 'Conra Bota Fogos',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ShadowCircularVolta' : 
+         {
+            'name' : 'Shadow Circular Volta',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'Maypole' : 
+         {
+            'name' : 'Maypole',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'ArgentineCrosses' : 
+         {
+            'name' : 'ArgentineCrosses',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'RollingOffTheArm' : 
+         {
+            'name' : 'Rolling Off The Arm',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'Plait' : 
+         {
+            'name' : 'Plait',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'BackRocks' : 
+         {
+            'name' : 'Back Rocks',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'OpenRocks' : 
+         {
+            'name' : 'Open Rocks',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'ClosedRocks' : 
+         {
+            'name' : 'Closed Rocks',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'CortaJaca' : 
+         {
+            'name' : 'Corta Jaca',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'ReverseTurn' : 
+         {
+            'name' : 'Reverse Turn',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'ShadowTravelingVolta' : 
+         {
+            'name' : 'Shadow Traveling Volta',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'FootChanges' : 
+         {
+            'name' : 'Foot Changes',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'SoloSpotVolta' : 
+         {
+            'name' : 'Solo Spot Volta',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'CrissCrossVoltas' : 
+         {
+            'name' : 'Criss Cross Voltas',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'BotaFogosToPromenadeAndCPP' : 
+         {
+            'name' : 'Bota Fogos To Promenade and Counter Promenade',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'TravelingBotaFogosBack' : 
+         {
+            'name' : 'Traveling Bota Fogos Back',
+            'level' : 'Bronze',
+            'follow' : [
+             ]
+        },
+        'CrissCrossBotaFogos' : 
+         {
+            'name' : 'Criss Cross Bota Fogos (Shadow Bota Fogos)',
+            'urlpath' : '/criss-cross-bota-fogos-shadow-bota-fogos',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+        'TravelingBotaFogosForward' : 
+         {
+            'name' : 'Traveling Bota Fogos Forward',
+            'urlpath' : '/traveling-bota-fogos-forward',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+        'VoltaMovements' : 
+         {
+            'name' : 'Volta Movements',
+            'urlpath' : '/volta-movements',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+        'RhythmBounce' : 
+         {
+            'name' : 'Rhythm Bounce',
+            'urlpath' : '/rhythm-bounce',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+        'SambaWalks' : 
+         {
+            'name' : 'SambaWalks',
+            'urlpath' : '/samba-walks',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+        'Whisks' : 
+         {
+            'name' : 'Whisk',
+            'urlpath' : '/whisk',
+            'level' : 'Newcomer',
+            'follow' : [
+             ]
+        },
+        'BasicMovements' : 
+         {
+            'name' : 'Basic Movements',
             'urlpath' : '/basic-movements',
             'level' : 'Newcomer',
             'follow' : [
-                {
-                   'id' : 'ProgressiveWalks',
-                   'comment' : 'back, after 1-3, when danced in Open or Closed Position'
-                },
-                {
-                   'id' : 'NaturalTop',
-                   'comment' : 'after 1-3 Open Basic, Man step 3 LF side turning slightly R, Lady RF fwd, danced in Open Position'
-                }
              ]
         }
     };
