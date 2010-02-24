443d702e8a182328101a9ab64cc188d9707cd228
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-02-24 02:16:40 +0000

    Adding tips from latin technique page.

--- trunk/dancetips/tips.js
+++ trunk/dancetips/tips.js
@@ -6,6 +6,82 @@
 */
     var danceTips = [
       {
+        'labels': ['Latin'],
+        'tip': 'back 2a: when weight is taken on back foot, point front foot, heel up, but with split weight.'
+      },
+      {
+        'labels': ['Latin', 'Rumba', 'Cha Cha Cha'],
+        'tip': 'fwd 2a: "2" is picture pose, keep both legs straight, 70-30 weight split, trailing foot turned out, heel up, do late checking action on "a", start moving back.'
+      },
+      {
+        'labels': ['Latin', 'Lady'],
+        'tip': 'Latin: strive to keep legs together, minimize space between thighs.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Latin: part of foot is in contact with floor with pressure at all times.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Latin turns: turn on ball of foot, while keep foot flat.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Trailing foot: break ankle, maintain downward pressure.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Turning steps: initiate body turn first, delay lifting foot, spot, then follow with sharp head and foot movement.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Hold the foot as long as music allows before stepping quickly into position for a sharp look.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Maintain pressure on floor for the free foot. Practice by stepping on and moving paper under foot.'
+      },
+      {
+        'labels': ['Latin', 'Standard'],
+        'tip': 'Moving foot from position to another, foot much track under body.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Latin forward step: the back foot will turn out naturally when weight is taken on front foot.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Latin forward and side step: feet always turned out.'
+      },
+      {
+        'labels': ['Latin', 'Cha Cha Cha'],
+        'tip': 'Cha Cha Cha: 2nd Chasse step is a lateral L and R hip movement, i.e. minimal rotation.'
+      },
+      {
+        'labels': ['Latin'],
+        'tip': 'Latin: Every step starts with a slight flexed knee. Knee straightens just before it gets to its position.'
+      },
+      {
+        'labels': ['Latin', 'Styling'],
+        'tip': 'Latin: arms should never be static.'
+      },
+      {
+        'labels': ['Latin', 'Frame'],
+        'tip': 'Latin: avoid drop-dead arms, frame is "up", round with energy.'
+      },
+      {
+        'labels': ['Latin', 'Frame'],
+        'tip': 'Latin Open Hold: arms more "out" than "down"'
+      },
+      {
+        'labels': ['Latin', 'Frame'],
+        'tip': 'Latin Closed Hold: forearms can touch or lean slightly forward, never bend backward. Maintain tone.'
+      },
+      {
+        'labels': ['Latin', 'Frame'],
+        'tip': "Latin: Lady's L hand is higher on Man's R arm,  around Man's R shoulder"
+      },
+      {
         'labels': ['Frame'],
         'tip': 'Shoulders down and flat, especially during and after turns.'
       },
