a519658b00b5510448059b3f0016566cc488bb6e
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-02-24 01:43:44 +0000

    Added mode to show all tips. Allow user to selecte label.

--- trunk/dancetips/dancetips.js
+++ trunk/dancetips/dancetips.js
@@ -1,3 +1,6 @@
+var labelFilters = {}; // in filter and with value "0" means not selected.
+var allLabels = [];
+
 //-------------------------------------------
 // one-time initialization after scripts are loaded
 function initVars() {
@@ -5,6 +8,18 @@ function initVars() {
     var paramValue = getURLParam('mode');
     if (paramValue) inputMode = paramValue;
   }
+
+  // get sorted labels
+  danceTips.forEach(function (tip) {
+    if (tip['labels'] && tip['labels'].length > 0) {
+      tip['labels'].forEach(function (label) {
+        if (!contains(allLabels, label)) {
+          allLabels.push(label);
+        }
+      });
+    }
+  });
+  allLabels.sort();
 }
      
 // initialize display
@@ -17,7 +32,8 @@ function initDisplay() {
 
     pickTip();
   } else { // full display
-    displayAllTips();
+    displayLabels();
+    showTips();
   }
 }
 
@@ -26,6 +42,7 @@ function pickTip() {
   var indexSelectedTip = Math.floor(Math.random() * lenTips);
 
   var output = [];
+
   // boiler plate
   output.push('<table width=100% style="background: #fff4c2;border:1px solid gray;border-collapse:collapse;"><tr><td style="border-collapse:collapse;padding:5px;">');
 
@@ -37,7 +54,8 @@ function pickTip() {
     output.push('">more</a>');
   }
 
-  output.push('&nbsp; <a style="float:right" href="javascript:void(0);" onclick="pickTip();">Next</a>');
+  output.push('&nbsp; <span style="float:right"><a href="javascript:void(0);" onclick="pickTip();">Another one</a>');
+  output.push('&nbsp; <a target="_top" href="http://www.dancecentral.info/ballroom/international-style/dance-tips">All</a>');
 
   // boiler plate
   output.push('</td></tr></table>');
@@ -45,16 +63,69 @@ function pickTip() {
   document.getElementById('idTipsBody').innerHTML = output.join('');
 }
 
-function displayAllTips() {
-  // simply display for now.
+function selectAll(enable) {
+  allLabels.forEach(function (label) {
+    labelFilters[label] = enable ? "1" : "0"; 
+  });
+
+  displayLabels();
+  showTips();
+}
+
+function toggleLabel(labelElement) {
+  var label = labelElement.innerHTML;
+
+  if (!labelFilters[label]) {
+    labelFilters[label] = "0";
+  } else {
+    labelFilters[label] = (labelFilters[label] == "0")  ? "1" : "0";
+  }
+
+  labelElement.className = isLabelSelected(label) ? 'selectedLabel' : 'nonSelectedLabel';
+  showTips();
+}
+
+function isLabelSelected(label) {
+  return !labelFilters[label] || labelFilters[label] == "1";
+}
+
+function isTipSelected(tip) {
+  if (!tip['labels']) return true;
+  for (var i = 0; i< tip['labels'].length; i++) {
+    if (isLabelSelected(tip['labels'][i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function displayLabels() {
   var output = [];
+
+  output.push('&nbsp;<b>Labels:</b> <a href="javascript:void(0);" onclick="selectAll(true);">select all</a>');
+  output.push('&nbsp; <a href="javascript:void(0);" onclick="selectAll(false);">deselect all</a><br>');
+
+  allLabels.forEach(function (label) {
+    output.push('<span class="'  +
+      (isLabelSelected(label) ? 'selectedLabel' : 'nonSelectedLabel') + 
+      '" onmouseover="this.style.cursor=\'pointer\';" onclick="toggleLabel(this);">' + label + '</span>');
+  });
+
+  output.push('<p><b>Tips:</b><br>');
+  document.getElementById('idTipsHeader').innerHTML = output.join('&nbsp; ');
+}
+
+function showTips() {
+  var output = [];
+
   output.push('<ol>');
   danceTips.forEach(function (tip) {
+    if (!isTipSelected(tip)) return;
     output.push('<li>' + tip['tip']);
     if (tip['labels'] && tip['labels'].length > 0) {
-      output.push(' (');
+      output.push(' [');
       output.push(tip['labels'].join(', '));
-      output.push(')');
+      output.push(']');
     }
     if (tip['urlpath']) {
       output.push('&nbsp; <a target="_blank" href="');
--- trunk/dancetips/tips.js
+++ trunk/dancetips/tips.js
@@ -6,30 +6,28 @@
 */
     var danceTips = [
       {
-        'labels': ['Standard', 'Latin'],
-        'figures': [],
-        'tip': 'Shoulders down and flat, especially after turns.',
-        'urlpath': 'http://www.dancecentral.info/ballroom/international-style/international-standard-technique'
+        'labels': ['Frame'],
+        'tip': 'Shoulders down and flat, especially during and after turns.'
       },
       {
         'labels': ['Standard'],
-        'tip': 'CBM: foot movement is the same, body turns.'
+        'tip': 'CBM: foot movement is the same direction, just add body turns.'
       },
       {
-        'labels': ['Standard'],
-        'tip': 'Lead normal back step straight back, lead OP back step toward your elbow.'
+        'labels': ['Standard', 'Man'],
+        'tip': 'Lead normal back step straight back, lead outside partner back step toward your elbow.'
       },
       {
         'labels': ['Standard'],
         'tip': 'Keep foot straight, not turned out in all standard dances. When turning, it is still straight fwd relative to body.'
       },
       {
-        'labels': ['Standard'],
-        'tip': 'Lady: stepping back, show your heel to gain even bigger stride.'
+        'labels': ['Standard', 'Lady'],
+        'tip': 'When stepping back, show your heel to gain even bigger stride.'
       },
       {
         'labels': ['Standard'],
-        'tip': 'Slowly roll onto the foot with control.',
-        'urlpath': 'http://www.dancecentral.info/ballroom/international-style/international-standard-technique'
+        'urlpath': 'http://www.dancecentral.info/ballroom/international-style/international-standard-technique',
+        'tip': 'Slowly roll onto the foot with control.'
       }
     ];
