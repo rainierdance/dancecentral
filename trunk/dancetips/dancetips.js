1fb52173d497ec81c63626424133ac7d5bd587d5
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-02-25 08:48:45 +0000

    Add general Latin and Standard technique

--- trunk/dancetips/dancetips.js
+++ trunk/dancetips/dancetips.js
@@ -9,17 +9,19 @@ function initVars() {
     if (paramValue) inputMode = paramValue;
   }
 
-  // get sorted labels
+  // get sorted labels, initialize filters
   danceTips.forEach(function (tip) {
     if (tip['labels'] && tip['labels'].length > 0) {
       tip['labels'].forEach(function (label) {
         if (!contains(allLabels, label)) {
           allLabels.push(label);
+          labelFilters[label] = "1";
         }
       });
     }
   });
   allLabels.sort();
+
 }
      
 // initialize display
@@ -33,7 +35,23 @@ function initDisplay() {
     pickTip();
   } else { // full display
     displayLabels();
+    displayTipsHeader();
     showTips();
+  }
+}
+
+function displayOneTip(output, tip) {
+  if (tip['labels'] && tip['labels'].length > 0) {
+    output.push('<span class="tipLabel">[');
+    output.push(tip['labels'].join(', '));
+    output.push(']</span> ');
+  }
+
+  output.push(tip['tip']);
+  if (tip['urlpath']) {
+    output.push('&nbsp; <a target="_blank" href="');
+    output.push(tip['urlpath']);
+    output.push('">more</a>');
   }
 }
 
@@ -47,13 +65,8 @@ function pickTip() {
   output.push('<table width=100% style="background: #fff4c2;border:1px solid gray;border-collapse:collapse;"><tr><td style="border-collapse:collapse;padding:5px;">');
 
   // actual tip
-  output.push(danceTips[indexSelectedTip]['tip']);
-  if (danceTips[indexSelectedTip]['urlpath']) {
-    output.push('&nbsp; <a target="_blank" href="');
-    output.push(danceTips[indexSelectedTip]['urlpath']);
-    output.push('">more</a>');
-  }
-
+  var tip = danceTips[indexSelectedTip];
+  displayOneTip(output, tip);
   output.push('&nbsp; <span style="float:right"><a href="javascript:void(0);" onclick="pickTip();">Another one</a>');
   output.push('&nbsp; <a target="_top" href="http://www.dancecentral.info/ballroom/international-style/dance-tips">All</a>');
 
@@ -75,28 +88,39 @@ function selectAll(enable) {
 function toggleLabel(labelElement) {
   var label = labelElement.innerHTML;
 
-  if (!labelFilters[label]) {
-    labelFilters[label] = "0";
-  } else {
-    labelFilters[label] = (labelFilters[label] == "0")  ? "1" : "0";
-  }
+  labelFilters[label] = (labelFilters[label] == "0")  ? "1" : "0";
 
   labelElement.className = isLabelSelected(label) ? 'selectedLabel' : 'nonSelectedLabel';
   showTips();
 }
 
 function isLabelSelected(label) {
-  return !labelFilters[label] || labelFilters[label] == "1";
+  return labelFilters[label] == "1";
 }
 
 function isTipSelected(tip) {
-  if (!tip['labels']) return true;
-  for (var i = 0; i< tip['labels'].length; i++) {
-    if (isLabelSelected(tip['labels'][i])) {
-      return true;
+  if (!tip['labels']) return true;  // no labels, considered match
+
+  var opAny = document.getElementById('opAny').checked;
+
+  var result = !opAny;
+  for (var label in labelFilters) {
+    if (labelFilters[label] == "1") {  // the label is selected
+      if (contains(tip['labels'], label)) { // tip contains label
+        if (opAny) {
+          result = true;
+          break;
+        }        
+      } else { // tip does not contain the label
+        if (!opAny) {
+          result = false;
+          break;
+        }        
+      }
     }
   }
-  return false;
+
+  return result;
 }
 
 function displayLabels() {
@@ -110,8 +134,15 @@ function displayLabels() {
       (isLabelSelected(label) ? 'selectedLabel' : 'nonSelectedLabel') + 
       '" onmouseover="this.style.cursor=\'pointer\';" onclick="toggleLabel(this);">' + label + '</span>');
   });
+  document.getElementById('idOptions').innerHTML = output.join('&nbsp; ');
+}
 
-  output.push('<p><b>Tips:</b><br>');
+function displayTipsHeader() {
+  var output = [];
+  output.push('<p><b>Tips:</b> (matching');
+  output.push('<input type="radio" id="opAny" name="op" value="any" checked onclick="showTips();"><b>any</b> &nbsp;');
+  output.push('<input type="radio" id="opAll" name="op" value="all" onclick="showTips();"><b>all</b>');
+  output.push(' of the selected labels)<br>');
   document.getElementById('idTipsHeader').innerHTML = output.join('&nbsp; ');
 }
 
@@ -121,17 +152,8 @@ function showTips() {
   output.push('<ol>');
   danceTips.forEach(function (tip) {
     if (!isTipSelected(tip)) return;
-    output.push('<li>' + tip['tip']);
-    if (tip['labels'] && tip['labels'].length > 0) {
-      output.push(' [');
-      output.push(tip['labels'].join(', '));
-      output.push(']');
-    }
-    if (tip['urlpath']) {
-      output.push('&nbsp; <a target="_blank" href="');
-      output.push(tip['urlpath']);
-      output.push('">more</a>');
-    }
+    output.push('<li>');
+    displayOneTip(output, tip);
   });
   output.push('</ol>');
   document.getElementById('idTipsBody').innerHTML = output.join('');
--- trunk/dancetips/tips.js
+++ trunk/dancetips/tips.js
@@ -1,44 +1,316 @@
 /*
       {
         'labels': ['Standard'],
-        'tip': ''
+        'tip': ""
+      },
+      {
+        'labels': ['Practice'],
+        'tip': ""
+      },
+      {
+        'labels': ['Latin'],
+        'tip': ""
       },
 */
     var danceTips = [
       {
-        'labels': ['Latin'],
-        'tip': 'back 2a: when weight is taken on back foot, point front foot, heel up, but with split weight.'
+        'labels': ['Practice'],
+        'tip': "The <a href='http://www.dancecentral.info/ballroom/international-style/international-standard-technique#TOC-Positions-and-Shaping-with-Partner'>13-step position and shaping practice</a> is a good warmup routine."
       },
       {
-        'labels': ['Latin', 'Rumba', 'Cha Cha Cha'],
-        'tip': 'fwd 2a: "2" is picture pose, keep both legs straight, 70-30 weight split, trailing foot turned out, heel up, do late checking action on "a", start moving back.'
+        'labels': ['Practice'],
+        'tip': "CBM: Step toward mirror with CBM, soft knees, shoulders and hips flat. Do not turn foot."
+      },
+      {
+        'labels': ['Practice'],
+        'tip': "Using WF: Step back with tip toe, slip weight, roll down onto ball of back foot till two feet are even, lower heel."
+      },
+      {
+        'labels': ['Practice'],
+        'tip': "Using WF: Step forward, slide heel fwd, split weight, roll fwd onto front foot, bring back foot fwd, settle, repeat."
+      },
+      {
+        'labels': ['Practice'],
+        'tip': "Step forward, slide heel forward, split weight, roll forward onto front foot, bring back foot fwd, settle, repeat."
+      },
+      {
+        'labels': ['Practice'],
+        'tip': "Lead and follow with only body contact, shoulders parallel. Move around room and change body orientation."
+      },
+      {
+        'labels': ['Practice'],
+        'tip': "Lie face down on floor, lift head and bend back."
+      },
+      {
+        'labels': ['Practice'],
+        'tip': "Stand in front of wall with some space, then try to fill the space, make contact with wall, while keeping head and foot position."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "The illusion of winding up before a turning figure is achieved by underturning body on preceding figure."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Heel turn: Last step, push off the back foot, the other foot does not leave the floor."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Heel turn: '2&' When completing the turn, weight is on balls of both feet. Foot rise, knee flexed to absorb foot rise."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Heel turn: Closing foot if parallel throughout, no weight, keep knees straight."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Heel turn: Man does early rise to lead heel turn. Lady keep the straight legs and not swing back."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Heel turn: as weight is transfered to back foot, front leg straighten, keep it straight, toe release, drag heel."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Heel turn: Starts with CBM, step straight back with toes slightly turned in."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Pivot turns: when stepping forward, think about shaping away from partner, follow L elbow."
+      },
+      {
+        'labels': ['Standard', 'Turns', 'Lady'],
+        'tip': "Pivot turn: Lady ends with two tracks, leaving room for Man to step."
+      },
+      {
+        'labels': ['Standard', 'Turns', 'Man'],
+        'tip': "Pivot Turns: Man ends with his RF on same track as LF, like on balance beam."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "3 types of rise: foot, leg, body."
+      },
+      {
+        'labels': ['Standard', 'Lady'],
+        'tip': "Lady never 'falls down', they move 'across'."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Keeping good frame does not mean it's locked up. Upper body and hips often move independently."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Keep hips flat, tucked under body when moving forward. Do not tilt and lean forward onto partner."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Keep both feet in contact with floor at all times for better control, lower slowly."
+      },
+      {
+        'labels': ['Standard', 'Quickstep', 'Man'],
+        'tip': "Keep body elevated on QQ timing to lead Lady into a lock step. "
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "CBM: give enough room for partner to step through."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Backward movement: after the moment with split weight, collect hips, drag front heel, pull body back with center."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Backward movement: think of moving center of body, sitting on chair."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Backward movement is initiated by bending knees while maintaining their alignment with feet which puts body weight behind feet."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Forward movement: there is the moment with split weight, front leg straight and slightly flexed back leg."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Forward movement: as soon as moving foot heel passing back toe, release back heel from floor."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Forward movement starts with bending knee down & forward over the toes while maintaining body alignment."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Prepartion last step: keep shape till foot passing, that's when body is flat before next CBM step."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Step foot straight and then swivel (except for Tango)."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Initiate movement from center."
+      },
+      {
+        'labels': ['Standard', 'Man', 'Frame'],
+        'tip': "Do not lift up chin to improve posture. Keep head upward with body."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "'Spiral up' the frame: Lady has slight twist to R to fit in frame such that Man doesn't break wrist."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Really strech the front to curve spine back."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Lady use the settling time to sense next lead. Man and Lady should match settling amount."
+      },
+      {
+        'labels': ['Standard'],
+        'tip': "Always collect, track foot under body before moving into place."
+      },
+      {
+        'labels': ['Standard', 'Turns'],
+        'tip': "Head follows elbow during turns."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Knee slight forward to counter balance bending back spine."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Stretch both elbows outward, shoulder blade squeezed down, tucked in, maintaiin strong elbows."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Do not isolate/separate/shift upper body left like in Latin. Curve body, not break body line."
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': "Do not bend neck back. Neck straight on shoulder. Bend spine back while keeping weight forward."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Relax left side, stretch right side, keep head, body and R leg in one line."
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': "Always maintain relative position with partner, especially after turns."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Weight is held forward, never backward."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Left shoulder: down, out, left and tucked in."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Curve spine to left and back. Think of high jump, 'tuck' under arm."
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': "Stack up block of weight on top of each other, no shifting."
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': "Increase topline by stretching L side forward, leftward."
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': "Lady projects leftward/upward in such a position that Man should not need to curve his R fingers."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Only Lady's thumb and middle finger touches Man's upper arm. Lift other fingers upward."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Lady's L elbow should be even with her shoulder."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "Lady's LH has slight twist to left, RH slight twist to right, so thumb and arm form a smooth line."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': "From the back, Lady's arm forms a smooth curved line. No popped up shoulder or dropped elbows."
+      },
+      {
+        'labels': ['Standard', 'Frame', 'Lady'],
+        'tip': 'Lady always projects forward, leftward and up.'
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': 'Preparation: Man draws Lady in. Lady gives the R arm, does not draw Man in using arm/shoulder.'
+      },
+      {
+        'labels': ['Standard', 'Frame'],
+        'tip': "5 points of contact: Man's LH, body contact, Man's RH, Lady's upper arm, Lady's LH"
+      },
+      {
+        'labels': ['Latin', 'Turns'],
+        'tip': 'Turns often start at the end of preceding step. Break "41" into "41&". Turn on "&".'
+      },
+      {
+        'labels': ['Latin', 'Turns'],
+        'tip': 'All forward/backward open turns: start with step passing under body, then ending side or back.'
+      },
+      {
+        'labels': ['Latin', 'Turns'],
+        'tip': 'Twisting: when turns are in hips only.'
+      },
+      {
+        'labels': ['Latin', 'Rumba', 'Cha Cha'],
+        'tip': 'Back step 2a: settle on "a", front foot naturally slide forward.'
+      },
+      {
+        'labels': ['Latin', 'Rumba', 'Cha Cha'],
+        'tip': 'Back step: when weight is taken on back foot, point front foot, heel up, but with split weight.'
+      },
+      {
+        'labels': ['Latin', 'Rumba', 'Cha Cha'],
+        'tip': 'Fwd 2a: "2" is picture pose, keep both legs straight, 70-30 weight, back foot turned out, heel up.'
+      },
+      {
+        'labels': ['Latin', 'Rumba', 'Cha Cha'],
+        'tip': 'Fwd 2a: do late checking action on "a", change direction to move back.'
       },
       {
         'labels': ['Latin', 'Lady'],
-        'tip': 'Latin: strive to keep legs together, minimize space between thighs.'
+        'tip': 'Strive to keep legs together, minimize space between thighs.'
       },
       {
-        'labels': ['Latin'],
-        'tip': 'Latin: part of foot is in contact with floor with pressure at all times.'
+        'labels': ['Latin', 'Foot'],
+        'tip': 'Part of foot is in contact with floor with pressure at all times.'
       },
       {
-        'labels': ['Latin'],
-        'tip': 'Latin turns: turn on ball of foot, while keep foot flat.'
+        'labels': ['Latin', 'Turns'],
+        'tip': 'Turn on ball of foot, while keep foot flat.'
       },
       {
-        'labels': ['Latin'],
+        'labels': ['Latin', 'Foot'],
         'tip': 'Trailing foot: break ankle, maintain downward pressure.'
       },
       {
-        'labels': ['Latin'],
-        'tip': 'Turning steps: initiate body turn first, delay lifting foot, spot, then follow with sharp head and foot movement.'
+        'labels': ['Latin', 'Turns'],
+        'tip': 'Initiate body turn first, delay moving foot, spot, then flip head, with sharp, quick foot movement.'
       },
       {
-        'labels': ['Latin'],
+        'labels': ['Latin', 'Foot'],
         'tip': 'Hold the foot as long as music allows before stepping quickly into position for a sharp look.'
       },
       {
-        'labels': ['Latin'],
+        'labels': ['Latin', 'Practice'],
+        'tip': 'Do <a href="http://www.dancecentral.info/ballroom/international-style/latin-technique#TOC-Body-separation-ribcage-exercises">rib cage exercise</a> everyday.'
+      },
+      {
+        'labels': ['Latin', 'Practice'],
+        'tip': 'Practice by stepping on paper and moving paper under foot.'
+      },
+      {
+        'labels': ['Latin', 'Foot'],
         'tip': 'Maintain pressure on floor for the free foot. Practice by stepping on and moving paper under foot.'
       },
       {
@@ -46,44 +318,44 @@
         'tip': 'Moving foot from position to another, foot much track under body.'
       },
       {
-        'labels': ['Latin'],
-        'tip': 'Latin forward step: the back foot will turn out naturally when weight is taken on front foot.'
+        'labels': ['Latin', 'Foot'],
+        'tip': 'Forward step: the back foot will turn out naturally when weight is taken on front foot.'
       },
       {
-        'labels': ['Latin'],
+        'labels': ['Latin', 'Foot'],
         'tip': 'Latin forward and side step: feet always turned out.'
       },
       {
-        'labels': ['Latin', 'Cha Cha Cha'],
-        'tip': 'Cha Cha Cha: 2nd Chasse step is a lateral L and R hip movement, i.e. minimal rotation.'
+        'labels': ['Latin', 'Cha Cha'],
+        'tip': '2nd Chasse step is a lateral L and R hip movement, i.e. minimal rotation.'
       },
       {
         'labels': ['Latin'],
-        'tip': 'Latin: Every step starts with a slight flexed knee. Knee straightens just before it gets to its position.'
+        'tip': 'Every step starts with a slight flexed knee. Knee straightens just before it gets to its position.'
       },
       {
         'labels': ['Latin', 'Styling'],
-        'tip': 'Latin: arms should never be static.'
+        'tip': 'Arms should never be static.'
       },
       {
         'labels': ['Latin', 'Frame'],
-        'tip': 'Latin: avoid drop-dead arms, frame is "up", round with energy.'
+        'tip': 'Aoid drop-dead arms, frame is "up", round with energy.'
       },
       {
         'labels': ['Latin', 'Frame'],
-        'tip': 'Latin Open Hold: arms more "out" than "down"'
+        'tip': 'Open Hold: arms more "out" than "down"'
       },
       {
         'labels': ['Latin', 'Frame'],
-        'tip': 'Latin Closed Hold: forearms can touch or lean slightly forward, never bend backward. Maintain tone.'
+        'tip': 'Closed Hold: forearms can touch or lean slightly forward, never bend backward. Maintain tone.'
       },
       {
         'labels': ['Latin', 'Frame'],
-        'tip': "Latin: Lady's L hand is higher on Man's R arm,  around Man's R shoulder"
+        'tip': "Lady's L hand is higher on Man's R arm,  around Man's R shoulder"
       },
       {
-        'labels': ['Frame'],
-        'tip': 'Shoulders down and flat, especially during and after turns.'
+        'labels': ['Standard', 'Frame'],
+        'tip': "Shoulders down and flat, don't tilt, especially during CBM and turns. (Exception: shaping)"
       },
       {
         'labels': ['Standard'],
@@ -94,16 +366,15 @@
         'tip': 'Lead normal back step straight back, lead outside partner back step toward your elbow.'
       },
       {
-        'labels': ['Standard'],
-        'tip': 'Keep foot straight, not turned out in all standard dances. When turning, it is still straight fwd relative to body.'
+        'labels': ['Standard', 'Foot'],
+        'tip': 'Step foot straight forward, not turned out even when turning (except for Tango). This is relative to hips.'
       },
       {
         'labels': ['Standard', 'Lady'],
-        'tip': 'When stepping back, show your heel to gain even bigger stride.'
+        'tip': 'When stepping back, start with toe touching floor, show your heel to gain even bigger stride.'
       },
       {
         'labels': ['Standard'],
-        'urlpath': 'http://www.dancecentral.info/ballroom/international-style/international-standard-technique',
         'tip': 'Slowly roll onto the foot with control.'
       }
     ];
