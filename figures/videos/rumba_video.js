0f9596412072fb71e623924a034be5b37e3f4878
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-03-27 23:30:28 +0000

    Add ability to show videos - per figure

--- figures/videos/rumba_video.js
+++ figures/videos/rumba_video.js
@@ -8,13 +8,18 @@ danceVideos = danceVideos.concat([
   {
     'title' : 'Rumba Closed Basic',
     'description' : 'Slavik and Karina does Rumba Closed Basic, narrated by Corky Ballas.',
-    'labels' : ['Rumba Technique'],
+    'labels' : ['BasicMovementsClosed'],
     'url' : 'http://www.youtube.com/v/lYFbYz1nPKU'
   },
   {
     'title' : 'Rumba Walk',
     'description' : 'Slavik and Karina does Rumba walk, narrated by Corky Ballas.',
-    'labels' : ['Rumba Technique'],
+    'labels' : [
+        'ProgressiveWalksFwd',
+        'ProgressiveWalksBack',
+        'ProgressiveWalksFwdRSP',
+        'ProgressiveWalksFwdLSP',
+        'ProgressiveWalksFwdRShadow'],
     'url' : 'http://www.youtube.com/v/oiNj-juu3pQ'
   }
 ]);
