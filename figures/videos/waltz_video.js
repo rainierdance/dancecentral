dac191518252ce51067712bb9e991a387cdf31c1
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-03-27 17:24:53 +0000

    Videos data file

---
+++ figures/videos/waltz_video.js
@@ -0,0 +1,21 @@
+danceVideos = danceVideos.concat([
+  {
+    'title' : 'Waltz Natural Spin Turn',
+    'description' : 'From ballroomdancers.com',
+    'labels' : ['NaturalSpinTurn'],
+    'url' : 'http://www.youtube.com/v/ssdbLOWqj3w'
+  },
+  {
+    'title' : 'Waltz Reverse Turn',
+    'description' : 'Andy & Wendy Wong from The Grand Ballroom',
+    'labels' : ['ReverseTurn'],
+    'url' : 'http://www.youtube.com/v/lmCaPPcDxR8'
+  },
+  {
+    'title' : 'Waltz Basic Weave',
+    'description' : 'Andy & Wendy Wong from The Grand Ballroom',
+    'labels' : ['BasicWeave'],
+    'url' : 'http://www.youtube.com/v/YlkmzsQ6wmU'
+  }
+]);
+
