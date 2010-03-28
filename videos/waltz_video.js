b7c32edc1d36362e219906605cd89c0455711c65
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-03-28 00:33:37 +0000

    Moved current video clips

--- videos/waltz_video.js
+++ videos/waltz_video.js
@@ -1,5 +1,29 @@
 danceVideos = danceVideos.concat([
   {
+    'title' : 'Waltz LF Closed Change',
+    'description' : 'Andy & Wendy Wong from The Grand Ballroom',
+    'labels' : ['ClosedChangeLF'],
+    'url' : 'http://www.youtube.com/v/1Iw4x7yEfZc'
+  },
+  {
+    'title' : 'Waltz Whisk',
+    'description' : 'From ballroomdancers.com',
+    'labels' : ['Whisk'],
+    'url' : 'http://www.youtube.com/v/eHOgrhxFW_o'
+  },
+  {
+    'title' : 'Waltz Chasse From Promenade',
+    'description' : 'From ballroomdancers.com',
+    'labels' : ['ChasseFromPP'],
+    'url' : 'http://www.youtube.com/v/AKha1aMcqs0'
+  },
+  {
+    'title' : 'Waltz Basic Weave',
+    'description' : 'Andy & Wendy Wong from The Grand Ballroom',
+    'labels' : ['BasicWeave'],
+    'url' : 'http://www.youtube.com/v/YlkmzsQ6wmU'
+  },
+  {
     'title' : 'Waltz Natural Spin Turn',
     'description' : 'From ballroomdancers.com',
     'labels' : ['NaturalSpinTurn'],
