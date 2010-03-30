6fb0f25456d76af8bc0e0bd00c025a1b52dab197
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-03-30 07:55:56 +0000

    moved rumba videos

--- trunk/videos/rumba_video.js
+++ trunk/videos/rumba_video.js
@@ -1,12 +1,144 @@
 danceVideos = danceVideos.concat([
   {
+    'title' : 'Rumba Opening Out to Right and Left',
+    'description' : 'Demonstrated by Andy & Wendy Wong from grandballroom.com.',
+    'labels' : ['OpeningOutToRightAndLeft'],
+    'url' : 'http://www.youtube.com/v/LabNqDm1UiQ'
+  },
+  {
+    'title' : 'Rumba Natural Top, Opening Out, Closed Hip Twist',
+    'description' : 'Demonstrated by Andy & Wendy Wong from grandballroom.com.',
+    'labels' : ['NaturalTop', 'NaturalOpeningOutMovement', 'ClosedHipTwist', 'Alemana'],
+    'url' : 'http://www.youtube.com/v/xqatK2FmMeQ'
+  },
+  {
+    'title' : 'Rumba Hockey Stick',
+    'description' : 'In Russian.',
+    'labels' : ['HockeyStick'],
+    'url' : 'http://www.youtube.com/v/gt-hoKMPEkA'
+  },
+  {
+    'title' : 'Rumba Fan Position',
+    'description' : 'Slavik and Karina. This is not how it is normally danced, though.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/P2FfUqrLXKY'
+  },
+  {
+    'title' : 'Rumba Cuban Rock',
+    'description' : 'Demonstrated by Andy & Wendy Wong from grandballroom.com.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/wxeeuUtzqVc'
+  },
+  {
+    'title' : 'Rumba Hand To Hand',
+    'description' : 'Slavik and Karina.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/UBgpFlnrINg'
+  },
+  {
+    'title' : 'Rumba Switch Turns',
+    'description' : 'Slavik and Karina.',
+    'labels' : ['Turn'],
+    'url' : 'http://www.youtube.com/v/8Uys2ErHky0'
+  },
+  {
+    'title' : 'Rumba Solo Spot Turns',
+    'description' : 'Slavik and Karina.',
+    'labels' : ['Turn'],
+    'url' : 'http://www.youtube.com/v/GDcOjf3Gf7I'
+  },
+  {
+    'title' : 'Rumba New York',
+    'description' : 'Slavik and Karina.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/fVanyxvSv1s'
+  },
+  {
+    'title' : 'Rumba Cucarachas',
+    'description' : 'In Russian.',
+    'labels' : ['Cucaracha'],
+    'url' : 'http://www.youtube.com/v/wuN-u5h1cMU'
+  },
+  {
+    'title' : 'Rumba Technique: Rotation',
+    'description' : 'In Russian. Demonstrate Rumba Basic.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/y_NH51mt_Ls'
+  },
+  {
+    'title' : 'Rumba Technique: Legs - 1',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/dtSs3RhkNAo'
+  },
+  {
+    'title' : 'Rumba Technique: Legs - 2',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/Y3lp7lvlY4s'
+  },
+  {
+    'title' : 'Rumba Technique: Arms - 3',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/EaekBbp6Taw'
+  },
+  {
+    'title' : 'Rumba Technique: Arms - 2',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/6LBwPEziKys'
+  },
+  {
+    'title' : 'Rumba Technique: Arms - 1',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/751uEEBY08I'
+  },
+  {
+    'title' : 'Rumba Technique: Contraction',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/alTI5KsMuaM'
+  },
+  {
+    'title' : 'Rumba Walk and Basic Technique',
+    'description' : 'In Russian.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/bMmabQzQnh4'
+  },
+  {
+    'title' : 'Rumba Basic Bronze Routine',
+    'description' : 'All bronze figures.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/XwOtNpMRXII'
+  },
+  {
+    'title' : 'Rumba  Routine',
+    'description' : 'Some basic figures, some are more advanced. By Alan Tornsberg & Serena Lecca',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/EF3mEbgCHsE'
+  },
+  {
+    'title' : 'Rumba Silver Routine',
+    'description' : 'Slavik and Karina.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/DrViOar0RDs'
+  },
+  {
+    'title' : 'Rumba Bronze Routine',
+    'description' : 'Slavik and Karina.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/VFSpUL-ciFw'
+  },
+  {
     'title' : 'Rumba Walk Warm Up',
     'description' : 'Slavik and Karina does Rumba warm up, narrated by Corky Ballas.',
-    'labels' : ['Rumba Technique'],
+    'labels' : ['Technique'],
     'url' : 'http://www.youtube.com/v/SGjEuiyfNg0'
   },
   {
-    'title' : 'Rumba Closed Basic',
+    'title' : 'Rumba Closed Basic Movement',
     'description' : 'Slavik and Karina does Rumba Closed Basic, narrated by Corky Ballas.',
     'labels' : ['BasicMovementsClosed'],
     'url' : 'http://www.youtube.com/v/lYFbYz1nPKU'
