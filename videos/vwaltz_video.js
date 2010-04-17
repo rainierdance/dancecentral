969dd1e187ff6c3371cf4b96805f0cf3c2a9daf8
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-04-17 03:58:58 +0000

    clean up data file

--- videos/vwaltz_video.js
+++
@@ -1,9 +0,0 @@
-danceVideos = danceVideos.concat([
-  {
-    'title' : 'Viennese Waltz: Reverse Turn',
-    'description' : 'Demonstrated by Monika.',
-    'labels' : ['ReverseTurn', 'VWaltz'],
-    'url' : 'http://www.youtube.com/v/qq4tZR627-0'
-  }
-]);
-
