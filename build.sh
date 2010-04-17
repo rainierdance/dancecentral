5027626270f262ba112b3b17c281af4c4bde724c
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-04-17 01:20:20 +0000

    combine datafiles to speed up loading

--- build.sh
+++ build.sh
@@ -6,12 +6,26 @@ if [ -z "$dirname" ]; then
   exit 1
 fi
 
+# concat all data files
+DATAFILE=$dirname/${dirname}_data.js
+echo '// ---- DO NOT EDIT ----' >>$DATAFILE
+datafiles=`ls $dirname/data`
+for file in $datafiles; do
+  cat $dirname/$file >>$DATAFILE
+done
+
 TMP=tmp.xml
 cat $dirname/gadget_boiler_top.txt >$TMP
 cat $dirname/index.html >>$TMP
 cat $dirname/gadget_boiler_bottom.txt >>$TMP
 
 # replace IS_GADGET, VERSION, URL
-sed -e 's/IS_GADGET = false/IS_GADGET = true/g' $TMP | sed -e "s/var VERSION = .*$/var VERSION = \"$TIMESTAMP\";/g" | sed -e "s#HREF=\"#HREF=\"http://dancecentral.googlecode.com/svn/trunk/$dirname/#" | sed -e "s#SRC=\"#SRC=\"http://dancecentral.googlecode.com/svn/trunk/$dirname/#" > $dirname.xml
+sed -e 's/IS_GADGET = false/IS_GADGET = true/g' $TMP | \
+sed -e "s/var VERSION = .*$/var VERSION = \"$TIMESTAMP\";/g" | \
+sed -e "s#HREF=\"#HREF=\"http://dancecentral.googlecode.com/svn/trunk/$dirname/#" | \
+sed -e "s#SRC=\"#SRC=\"http://dancecentral.googlecode.com/svn/trunk/$dirname/#" | \
+sed -e "/src=\"data/d" | \
+sed -e "s#<!--DATA-->#<script type=\"text/javascript\" SRC=\"http://dancecentral.googlecode.com/svn/trunk/$DATAFILE\"></script>#" \
+> $dirname.xml
 
 rm $TMP
---
+++ videos/data/ballroom_video.js
@@ -0,0 +1,46 @@
+danceVideos = danceVideos.concat([
+  {
+    'title' : 'Standard Technique by Glen Weiss',
+    'description' : 'Glen Weiss, 2-time World Standard Champion, talks about how to get in correct frame. Waltz is danced as an example.<ul>' +
+      '<li> Stand with feet together, arms to side'+ 
+      '<li> Relax left side' +
+      '<li> Level our arms' +
+      '<li> Open our feet' +
+      '<li> Flex our knees slightly forward ' +
+      '</ul>',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/kW-ffl7i5-c'
+  },
+  {
+    'title' : 'Standard Technique for Ladies by Heather Smith - Vol 1',
+    'description' : 'Heather talks about center balance and stretch:<ul><li>Find center and diagnoal stretch using a chair: stretch center back to the spin; stretch left side and arm diagonally leftwards.' +
+        '<li>Line up 4 building blocks: head, shoulder, body, hips when stretching to side, both sides stretching evenly, do not break opposite side.</ul>',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/cAjqm8pyHZY'
+  },
+  {
+    'title' : 'Latin Styling Demonstration',
+    'description' : 'Demonstrated using Cha Cha. 5 points on leg action, body rhythm, arm styling, ronde, swivels.',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/w98G0tPWTPQ',
+  },
+  {
+    'title' : 'Latin Styling - Arm styling in Cha Cha by Jaana Knunitz',
+    'description' : 'Includes different timing',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/MKWHgdHoqu8',
+  },
+  {
+    'title' : 'Latin Styling - Arms, by Jaana Knunitz',
+    'description' : 'Arm styling in Rumba',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/3LQQL7_njnI'
+  },
+  {
+    'title' : 'Latin Styling - Core Isolations by Jaana Knunitz',
+    'description' : 'All about foot',
+    'labels' : [],
+    'url' : 'http://www.youtube.com/v/pM0Lk4NuMlA'
+  }
+]);
+
