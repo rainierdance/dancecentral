bea9634234666ba51c00471bc309fcbf2feea24f
jackiewu0910 <jackiewu0910@be6b1324-055e-11df-af7f-9df10b504a6d>
2010-09-30 17:03:22 +0000

    jive follows up to bronze

--- trunk/figures/figures_data.js
+++ trunk/figures/figures_data.js
@@ -2721,10 +2721,109 @@ var foxtrotFigures =
             'urlpath' : 'foo',
             'level' : 'Newcomer',
             'follow' : [
+              {
+                'id' : 'foo',
+                'comment' : ''
+              },
+              {
+                'id' : 'foo',
+                'comment' : ''
+              }
              ]
         },
 */
     var jiveFigures = {
+        'StalkingWalksFlicksAndBreak' : 
+         {
+            'name' : 'Stalking Walks, Flicks and Break',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'Catapult' : 
+         {
+            'name' : 'Catapult',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ChickenWalks' : 
+         {
+            'name' : 'Chicken Walks',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'Chugging' : 
+         {
+            'name' : 'Chugging',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ToeHeelSwivels' : 
+         {
+            'name' : 'Toe Heel Swivels',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'ShoulderSpin' : 
+         {
+            'name' : 'Shoulder Spin',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'CurlyWhip' : 
+         {
+            'name' : 'Curly Whip',
+            'level' : 'Gold',
+            'follow' : [
+             ]
+        },
+        'MiamiSpecial' : 
+         {
+            'name' : 'Miami Special',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'SimpleSpin' : 
+         {
+            'name' : 'Simple Spin',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'RollingOffTheArm' : 
+         {
+            'name' : 'Rolling Off the Arm',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'SpanishArms' : 
+         {
+            'name' : 'Spanish Arms',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'Windmill' : 
+         {
+            'name' : 'Windmill',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
+        'ReverseWhip' : 
+         {
+            'name' : 'Reverse Whip',
+            'level' : 'Silver',
+            'follow' : [
+             ]
+        },
         'FallawayRock' : 
          {
             'name' : 'Fallaway Rock',
@@ -2733,6 +2832,46 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'BasicInPlace',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayRock',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayThrowaway',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesRightToLeft',
+                'comment' : ''
+              },
+              {
+                'id' : 'Walks',
+                'comment' : ''
+              },
+              {
+                'id' : 'Mooch',
+                'comment' : ''
+              },
+              {
+                'id' : 'Whip',
+                'comment' : 'after 1-5'
+              },
+              {
+                'id' : 'WhipThrowaway',
+                'comment' : 'after 1-5'
+              },
+              {
+                'id' : 'ReverseWhip',
+                'comment' : ''
+              },
+              {
+                'id' : 'StalkingWalksFlicksAndBreak',
+                'comment' : ''
+              }
              ]
         },
         'FallawayThrowaway' : 
@@ -2743,6 +2882,34 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'Link',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : ''
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : ''
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'from L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChickenWalks',
+                'comment' : ''
+              }
              ]
         },
         'Link' : 
@@ -2753,6 +2920,46 @@ var foxtrotFigures =
             'startAlignment' : ['Open Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'BasicInPlace',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayRock',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayThrowaway',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesRightToLeft',
+                'comment' : ''
+              },
+              {
+                'id' : 'Walks',
+                'comment' : ''
+              },
+              {
+                'id' : 'Mooch',
+                'comment' : ''
+              },
+              {
+                'id' : 'Whip',
+                'comment' : 'after 1-5'
+              },
+              {
+                'id' : 'WhipThrowaway',
+                'comment' : 'after 1-5'
+              },
+              {
+                'id' : 'ReverseWhip',
+                'comment' : ''
+              },
+              {
+                'id' : 'StalkingWalksFlicksAndBreak',
+                'comment' : ''
+              }
              ]
         },
         'ChangeOfPlacesRightToLeft' : 
@@ -2763,6 +2970,50 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'Link',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : ''
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : ''
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'from L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : 'with change of hands when Change of Places R to L is danced with change of hands'
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'when Change of Places R to L is danced with change of hands'
+              },
+              {
+                'id' : 'MiamiSpecial',
+                'comment' : 'when Change of Places R to L is danced with change of hands'
+              },
+              {
+                'id' : 'ShoulderSpin',
+                'comment' : 'when Change of Places R to L is danced with change of hands'
+              },
+              {
+                'id' : 'Catapult',
+                'comment' : 'when Change of Places R to L is danced with change of hands'
+              }
              ]
         },
         'ChangeOfPlacesLeftToRight' : 
@@ -2773,6 +3024,62 @@ var foxtrotFigures =
             'startAlignment' : ['Open Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'Link',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : ''
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'from L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : ''
+              },
+              {
+                'id' : 'Windmill',
+                'comment' : ''
+              },
+              {
+                'id' : 'SpanishArms',
+                'comment' : ''
+              },
+              {
+                'id' : 'RollingOffTheArm',
+                'comment' : ''
+              },
+              {
+                'id' : 'SimpleSpin',
+                'comment' : 'when Change of Places L to R is end in Open CPP'
+              },
+              {
+                'id' : 'StalkingWalksFlicksAndBreak',
+                'comment' : 'starting in open position'
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'when Change of Places L to R is dance with change of hands'
+              },
+              {
+                'id' : 'RollingOffTheArm',
+                'comment' : 'R to R hand hold, when Change of Places L to R is dance with change of hands'
+              },
+              {
+                'id' : 'MiamiSpecial',
+                'comment' : 'when Change of Places L to R is dance with change of hands'
+              },
+              {
+                'id' : 'ShoulderSpin',
+                'comment' : 'when Change of Places L to R is dance with change of hands'
+              },
+              {
+                'id' : 'Catapult',
+                'comment' : 'when Change of Places L to R is dance with change of hands'
+              }
              ]
         },
         'ChangeOfHandsBehindBack' : 
@@ -2783,6 +3090,30 @@ var foxtrotFigures =
             'startAlignment' : ['Open Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'Link',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : ''
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : ''
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'from L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : ''
+              }
              ]
         },
         'HipBump' : 
@@ -2793,6 +3124,30 @@ var foxtrotFigures =
             'startAlignment' : ['Open Position'],
             'timing' : 'QQ QaQ a QaQ',
             'follow' : [
+              {
+                'id' : 'Link',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : ''
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : ''
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'from L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : ''
+              }
              ]
         },
         'AmericanSpin' : 
@@ -2803,6 +3158,54 @@ var foxtrotFigures =
             'startAlignment' : ['Open Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : 'with change of hands'
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : ''
+              },
+              {
+                'id' : 'RollingOffTheArm',
+                'comment' : 'with R to R hand hold'
+              },
+              {
+                'id' : 'MiamiSpecial',
+                'comment' : ''
+              },
+              {
+                'id' : 'ShoulderSpin',
+                'comment' : ''
+              },
+              {
+                'id' : 'Chugging',
+                'comment' : ''
+              },
+              {
+                'id' : 'Catapult',
+                'comment' : ''
+              },
+              {
+                'id' : 'Link',
+                'comment' : 'when American Spin ends with L to R hand hold'
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : 'when American Spin ends with L to R hand hold'
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : 'when American Spin ends with L to R hand hold'
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : 'when American Spin ends with L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : 'when American Spin ends with L to R hand hold'
+              }
              ]
         },
         'Walks' : 
@@ -2813,6 +3216,22 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ QaQ SS QQQQ',
             'follow' : [
+              {
+                'id' : 'FallawayThrowaway',
+                'comment' : '3-8'
+              },
+              {
+                'id' : 'ChangeOfPlacesRightToLeft',
+                'comment' : '3-8'
+              },
+              {
+                'id' : 'Whip',
+                'comment' : 'Chasse diag fwd LRL, turning 3/8 to R to end LF side and slightly fwd facing Lady to follow with Whip (Lady chasse fwd making no turn)'
+              },
+              {
+                'id' : 'WhipThrowaway',
+                'comment' : 'Chasse diag fwd LRL, turning 3/8 to R to end LF side and slightly fwd facing Lady to follow with Whip (Lady chasse fwd making no turn)'
+              }
              ]
         },
         'StopAndGo' : 
@@ -2823,6 +3242,30 @@ var foxtrotFigures =
             'startAlignment' : ['Open Position'],
             'timing' : 'QQ QaQ QQ QaQ',
             'follow' : [
+              {
+                'id' : 'Link',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfHandsBehindBack',
+                'comment' : ''
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : ''
+              },
+              {
+                'id' : 'AmericanSpin',
+                'comment' : 'with L to R hand hold'
+              },
+              {
+                'id' : 'StopAndGo',
+                'comment' : ''
+              }
              ]
         },
         'Mooch' : 
@@ -2833,6 +3276,30 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QQQQ QQ QaQ QQ QQQQQQ QaQ',
             'follow' : [
+              {
+                'id' : 'BasicInPlace',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayRock',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayThrowaway',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesRightToLeft',
+                'comment' : ''
+              },
+              {
+                'id' : 'Walks',
+                'comment' : ''
+              },
+              {
+                'id' : 'StalkingWalksFlicksAndBreak',
+                'comment' : ''
+              }
              ]
         },
         'Whip' : 
@@ -2843,6 +3310,34 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ',
             'follow' : [
+              {
+                'id' : 'FallawayRock',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayThrowaway',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesRightToLeft',
+                'comment' : ''
+              },
+              {
+                'id' : 'Walks',
+                'comment' : ''
+              },
+              {
+                'id' : 'Mooch',
+                'comment' : ''
+              },
+              {
+                'id' : 'ReverseWhip',
+                'comment' : ''
+              },
+              {
+                'id' : 'CurlyWhip',
+                'comment' : 'when Whip ends in Closed Position'
+              }
              ]
         },
         'WhipThrowaway' : 
@@ -2853,6 +3348,14 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ',
             'follow' : [
+              {
+                'id' : 'ChangeOfPlacesLeftToRight',
+                'comment' : 'Man turn 1/4 R over 3-6, Lady 5/8 L'
+              },
+              {
+                'id' : 'HipBump',
+                'comment' : 'Man turn 1/8 R over 3-5, Lady 1/4 L'
+              }
              ]
         },
         'BasicInPlace' : 
@@ -2863,6 +3366,38 @@ var foxtrotFigures =
             'startAlignment' : ['Closed Position'],
             'timing' : 'QQ QaQ QaQ',
             'follow' : [
+              {
+                'id' : 'BasicInPlace',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayRock',
+                'comment' : ''
+              },
+              {
+                'id' : 'FallawayThrowaway',
+                'comment' : ''
+              },
+              {
+                'id' : 'ChangeOfPlacesRightToLeft',
+                'comment' : ''
+              },
+              {
+                'id' : 'Walks',
+                'comment' : ''
+              },
+              {
+                'id' : 'Mooch',
+                'comment' : ''
+              },
+              {
+                'id' : 'ReverseWhip',
+                'comment' : ''
+              },
+              {
+                'id' : 'StalkingWalksFlicksAndBreak',
+                'comment' : 'Stalking Walks'
+              }
              ]
         }
     };
