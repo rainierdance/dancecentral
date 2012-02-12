9becf1b36238ba73d7bcf8c0dc061a1e4d076a6b
jackiewu0910@gmail.com <jackiewu0910@gmail.com@be6b1324-055e-11df-af7f-9df10b504a6d>
2012-02-12 08:29:15 +0000

    class viewer

---
+++ trunk/regclass/localonly.js
@@ -0,0 +1,45 @@
+<!--Only included for local test -->
+function onLoad() {
+  showFilters();
+}
+
+function _gel(id) {
+  return document.getElementById(id);
+}
+
+function escapeHtml(text) {
+  if (text == null) {
+    return '';
+  }
+  //return _hesc(text);
+  return text;
+}
+
+
+// initialization for local testing
+gAllClasses.push({
+  'title' : 'Monday 8:00pm Intl. Latin Level 2 - Jive',
+  'category' : 'Series',
+  'start_date': '3/5/2012',
+  'day_of_week': 'MON',
+  'start_time': '8:00 PM',
+  'style': 'International Latin',
+  'dance_name': 'Jive',
+  'level':'Level 2',
+  'description': 'ISTD syllabus figures and technique for Jive at the "Associate" or "Bronze" level. Prerequisite is Level 1 Jive.',
+  'teacher' : 'Kora',
+  'bio' : 'http://foo'
+  });
+gAllClasses.push({
+  'title' : 'Intl. Latin Level 1 - Rumba',
+  'category' : 'DropIn',
+  'start_date': '2/5/2012',
+  'day_of_week': 'TUE',
+  'start_time': '2:00 PM',
+  'style': 'International Latin',
+  'dance_name': 'Rumba',
+  'level':'Level 1',
+  'description': 'ISTD syllabus figures and technique for Jive at the "Associate" or "Bronze" level. Prerequisite is Level 1 Jive.',
+  'teacher' : 'Simeon',
+  'bio' : 'http://foo'
+  });
