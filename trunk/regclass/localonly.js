cb02731867c49398e081cc713ade88c93cda6adf
jackiewu0910@gmail.com <jackiewu0910@gmail.com@be6b1324-055e-11df-af7f-9df10b504a6d>
2012-02-14 06:12:06 +0000

    match empty

--- trunk/regclass/localonly.js
+++ trunk/regclass/localonly.js
@@ -17,14 +17,14 @@ function escapeHtml(text) {
 
 // initialization for local testing
 gAllClasses.push({
-  'title' : 'Monday 8:00pm Intl. Latin Level 2 - Jive',
+  'title' : 'Monday 8:00pm Intl. Latin Level 2 - Jive ANY',
   'category' : 'Series',
   'start_date': '3/5/2012',
   'day_of_week': 'MON',
   'start_time': '8:00 PM',
   'style': 'International Latin, International Standard',
   'dance_name': 'Jive',
-  'level':'Level 2',
+  'level':'',
   'description': 'ISTD syllabus figures and technique for Jive at the "Associate" or "Bronze" level. Prerequisite is Level 1 Jive.',
   'teacher' : '',
   'bio' : 'http://foo'
@@ -63,7 +63,7 @@ gAllClasses.push({
   'start_time': '2:00 PM',
   'style': 'International Standard',
   'dance_name': 'Rumba',
-  'level':'Level 1',
+  'level':'Level 2',
   'description': 'ISTD syllabus figures and technique for Jive at the "Associate" or "Bronze" level. Prerequisite is Level 1 Jive.',
   'teacher' : 'Simeon',
   'bio' : 'http://foo'
