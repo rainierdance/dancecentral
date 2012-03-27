0edb0a4c6e9c3d11647e7467aaba9183d9f3630a
jackiewu0910@gmail.com <jackiewu0910@gmail.com@be6b1324-055e-11df-af7f-9df10b504a6d>
2012-03-27 20:52:17 +0000

    UI update, show report time

--- studiostats/localonly.js
+++ studiostats/localonly.js
@@ -23,7 +23,7 @@ gAllRawEntities.push("2/22/2012 0:04:01;Rachel Wu;Group;2/22/2012;;Series Monday
 gAllRawEntities.push("2/22/2012 15:47:58;Lorenzo Farris;Group;2/22/2012;15:47;Series 3/5/2012 Wednesday 8:00 PM Intl. Latin Level 3 - Paso Doble;;;Paid;;;;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/22/2012 15:48:18;Lorenzo Farris;Group;2/22/2012;15:48;Party Thursday 9:30 PM Ballroom Practice Party;;;Paid;;;Cash;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/22/2012 15:48:25;Lorenzo Farris;Youth;2/22/2012;15:48;Children 3/5/2012 Wednesday 4:00 PM Children's Program Level 1;;;;;;;;;;;;;;;;;;;;;;;;;".split(';'));
-gAllRawEntities.push("2/27/2012 11:02:03;Violeta Abramova;Teach;2/27/2012;11:01;Children 2012-02-27 Monday 4:00 PM Children's Program Level 1;;;;;;;;;;;;;;;;;;;;;;;;;".split(';'));
+gAllRawEntities.push("2/27/2012 11:02:03;Violeta Abramova;Teach;2/27/2012;11:01;Series Wednesday 8:00 PM Am. Rhythm Level 2 - Rumba,Children 2012-02-27 Monday 4:00 PM Children's Program Level 1;;;;;;;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/23/2012 15:48:29;Lorenzo Farris;Private;2/22/2012;15:48;;1;Simeon Stoynov;;;;;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/24/2012 15:48:35;Lorenzo Farris;Practice;2/22/2012;15:48;;1;;Practice-Free;TODO practice;;;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/22/2012 15:48:46;Lorenzo Farris;Payment;2/22/2012;15:48;;;;;Special;65;Check;;;3;;;;;;;;65;;;;;;Wedding Special;;".split(';'));
@@ -50,3 +50,22 @@ gAllRawEntities.push("2/22/2012 18:27:03;Jackie Wu;Group;2/22/2012;18:26;Series 
 gAllRawEntities.push("2/27/2012 10:14:35;Jackie Wu;Private;2/27/2012;10:14;;1;Simeon Stoynov;;TODO here;;;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/27/2012 10:14:39;Jackie Wu;Private;2/22/2012;10:14;;1;Simeon Stoynov;;;;;;;;;;;;;;;;;;;;;;;".split(';'));
 gAllRawEntities.push("2/27/2012 10:48:14;Jackie Wu;Youth;2/27/2012;10:47;Children_2012_03_05_MON_LEVEL_1,Youth_2012_03_05_WED_17_30_LEVEL_1;;;;TODO some;;;;;;;;;;;;;;;;;;;;;".split(';'));
+
+gAllRawEntities.push("3/5/2012 23:32:35;Cecilia Wang;Private;3/2/2012;19:30:00;;60;Simeon Stoynov".split(';'));
+gAllRawEntities.push("3/16/2012 13:19:36;Cecilia Wang;Payment;3/2/2012;13:18:00;;;;;EDS floor fee;16;;;;1.33;;;;;;;;16".split(';'));
+gAllRawEntities.push("3/12/2012 17:59:18;Cecilia Wang;Private;3/7/2012;18:30:00;;45;Simeon Stoynov;;with Ed".split(';'));
+gAllRawEntities.push("3/16/2012 13:21:30;Cecilia Wang;Payment;3/7/2012;13:21:00;;;;;EDS floor fee;12;;;;1;;;;;;;;12".split(';'));
+gAllRawEntities.push("3/11/2012 15:38:04;Cecilia Wang;Group;3/8/2012;20:10:00;Drop-in 2012-03-08 Thursday 8:00 PM Standard Technique;;;Free".split(';'));
+gAllRawEntities.push("3/16/2012 14:39:26;Cecilia Wang;Private;3/14/2012;18:50:00;;45;Kora Stoynova".split(';'));
+gAllRawEntities.push("3/25/2012 17:17:57;Cecilia Wang;Payment;3/14/2012;18:50:00;;;;;EDS floor fee;12;;;;1;;;;;;;;12".split(';'));
+
+gAllRawEntities.push("3/8/2012 12:03:59;Diane Johnson;Private;3/8/2012;12:15:00;;45;Simeon Stoynov".split(';'));
+gAllRawEntities.push("3/8/2012 12:03:59;Diane Johnson;Private;3/8/2012;13:00:00;;45;Simeon Stoynov".split(';'));
+gAllRawEntities.push("3/16/2012 13:21:58;Diane Johnson;Payment;3/8/2012;13:21:00;;;;;EDS floor fee;24;;;;2;;;;;;;;24".split(';'));
+gAllRawEntities.push("3/16/2012 14:52:02;Diane Johnson;Private;3/15/2012;11:45:00;;45;Simeon Stoynov".split(';'));
+gAllRawEntities.push("3/21/2012 14:17:31;Diane Johnson;Private;3/15/2012;12:45:00;;60;Simeon Stoynov".split(';'));
+gAllRawEntities.push("3/22/2012 12:44:01;Diane Johnson;Private;3/22/2012;23:45:00;;120;Simeon Stoynov".split(';'));
+gAllRawEntities.push("3/25/2012 17:01:14;Diane Johnson;Payment;3/15/2012;11:45:00;;;;;EDS floor fee;12;;;;1;;;;;;;;12".split(';'));
+gAllRawEntities.push("3/25/2012 17:01:44;Diane Johnson;Payment;3/15/2012;13:45:00;;;;;EDS floor fee;16;;;;1.33;;;;;;;;16".split(';'));
+gAllRawEntities.push("3/26/2012 17:01:44;Diane Johnson;Payment;3/16/2012;13:45:00;;;;;EDS floor fee;32;;;;2.67;;;;;;;;32".split(';'));
+
