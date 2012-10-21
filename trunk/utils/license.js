202277cbd5ea57b8123bed69b18d2ce54cad24a0
jackiewu0910@gmail.com <jackiewu0910@gmail.com@be6b1324-055e-11df-af7f-9df10b504a6d>
2012-10-21 01:03:10 +0000

    adding powered by

---
+++ trunk/utils/license.js
@@ -0,0 +1,10 @@
+//-------------------------------------------
+function showCopyright() {
+  var element = document.getElementById('idCopyright');
+  if (!element) return;
+  
+  // show copyright
+  var html = '<span style="float:right">Powered By <a href="http://www.rainierdance.com" target="_blank"><img border=0 height=16 src="../images/rainierdance_logo.jpg"/></> <a href="http://www.rainierdance.com" target="_blank">Rainier Dance, LLC.</a>' +
+    ' <a href="http://www.rainierdance.com/license" target="_blank">Terms</a></span>';
+  element.innerHTML = html;
+}
