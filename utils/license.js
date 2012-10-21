0a88574d5a74928d492bfc18bb42b5ec25698b28
jackiewu0910@gmail.com <jackiewu0910@gmail.com@be6b1324-055e-11df-af7f-9df10b504a6d>
2012-10-21 06:36:56 +0000

    update logo location

--- utils/license.js
+++ utils/license.js
@@ -4,7 +4,7 @@ function showCopyright() {
   if (!element) return;
   
   // show copyright
-  var html = '<span style="float:right">Powered By <a href="http://www.rainierdance.com" target="_blank"><img border=0 height=16 src="../images/rainierdance_logo.jpg"/></> <a href="http://www.rainierdance.com" target="_blank">Rainier Dance, LLC.</a>' +
+  var html = '<span style="float:right">Powered By <a href="http://www.rainierdance.com" target="_blank"><img border=0 height=16 src="https://dancecentral.googlecode.com/svn/trunk/images/rainierdance_logo.jpg"/></> <a href="http://www.rainierdance.com" target="_blank">Rainier Dance, LLC.</a>' +
     ' <a href="http://www.rainierdance.com/license" target="_blank">Terms</a></span>';
   element.innerHTML = html;
 }
