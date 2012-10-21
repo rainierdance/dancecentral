9fb1317d2495220ae2e940a6facff161717813c4
jackiewu0910@gmail.com <jackiewu0910@gmail.com@be6b1324-055e-11df-af7f-9df10b504a6d>
2012-10-21 17:35:05 +0000

    add license boiler plat

--- closeday/localonly.js
+++ closeday/localonly.js
@@ -3,12 +3,3 @@ function _gel(id) {
   return document.getElementById(id);
 }
 
-function escapeHtml(text) {
-  if (text == null) {
-    return '';
-  }
-  //return _hesc(text);
-  return text;
-}
-
-
